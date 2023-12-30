a3aaafefe1e39c8f265ad331edd015637a962f15
efilnukefesin <efil.threat@googlemail.com>
2023-12-30 22:48:53 +0100

    first commit

---
+++ src/SVRGN.Libs.Contracts.Base.IO/IFileIOWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SVRGN.Libs.Contracts.Base.IO
+{
+    public interface IFileIOWrapper
+    {
+        #region Methods
+        bool Exists(string path);
+        string ReadAllText(string path);
+        bool WriteAllText(string path, string text);
+
+        List<string> DirectoryGetFiles(string Path);
+        Stream ReadStream(string Filename);
+        bool WriteStream(string Filename, Stream Input);
+        #endregion Methods
+    }
+}
